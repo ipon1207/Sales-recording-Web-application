63f85885f2c7aec873e794c0d5de000df0d2281b
ipon <iphoneipon1207@gmail.com>
2024-11-25 18:14:15 +0900

    モデルの変更

--- src/main/java/com/example/salesrecordsystem/model/User.java
+++ src/main/java/com/example/salesrecordsystem/model/User.java
@@ -1,5 +1,40 @@
 package com.example.salesrecordsystem.model;
 
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
 public class User {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Integer userId;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "mail")
+    private String mail;
+
+    @Column(name = "group_id")
+    private Integer groupId;
+
+    @Column(name = "group_name")
+    private String groupName;
+
+    @Column(name = "created_at")
+    private Date createdAt;
 }
