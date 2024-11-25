8a5a8be65ff834e61515338331a6d49a98f15871
ipon <iphoneipon1207@gmail.com>
2024-11-25 15:08:10 +0900

    second commit

---
+++ src/main/java/com/example/salesrecordsystem/SalesrecordsystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.salesrecordsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesrecordsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesrecordsystemApplication.class, args);
+	}
+
+}
