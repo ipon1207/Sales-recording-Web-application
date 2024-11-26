6b6bd5eec29f27d77c95c90144680fbf3eb435b4
ipon <iphoneipon1207@gmail.com>
2024-11-26 16:08:32 +0900

    signup.htmlへの画面遷移と入力値のDB登録メソッドを実装(エラーあり)

--- src/main/java/com/example/salesrecordsystem/controller/SignupController.java
+++ src/main/java/com/example/salesrecordsystem/controller/SignupController.java
@@ -1,5 +1,27 @@
 package com.example.salesrecordsystem.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.salesrecordsystem.model.User;
+
+@Controller
 public class SignupController {
     
+    @GetMapping("/")
+    public String getRoot() {
+        // signup.htmlへ画面遷移
+        return "/user/signup";
+    }
+
+    @PostMapping("/signup")
+    public String postSignup(@ModelAttribute User user, Model model) {
+        // 入力値をデータベースに登録
+        service.insert(user);
+
+        return "result";
+    }
 }
