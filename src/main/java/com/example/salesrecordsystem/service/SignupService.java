eadd51d687b57d4624c7ee42e3f4afa1b548bba6
ipon <iphoneipon1207@gmail.com>
2024-11-26 16:22:01 +0900

    アノテーションを修正

--- src/main/java/com/example/salesrecordsystem/service/SignupService.java
+++ src/main/java/com/example/salesrecordsystem/service/SignupService.java
@@ -1,12 +1,12 @@
 package com.example.salesrecordsystem.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 import com.example.salesrecordsystem.model.User;
 import com.example.salesrecordsystem.repository.SignupRepository;
 
-@Repository
+@Service
 public class SignupService {
     
     @Autowired
