61f14329e5aeecb93917a584a2fb901f611c532c
ipon <iphoneipon1207@gmail.com>
2024-11-26 16:18:41 +0900

    データベースに値を登録するメソッドを実装

--- src/main/java/com/example/salesrecordsystem/service/SignupService.java
+++ src/main/java/com/example/salesrecordsystem/service/SignupService.java
@@ -1,5 +1,19 @@
 package com.example.salesrecordsystem.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.example.salesrecordsystem.model.User;
+import com.example.salesrecordsystem.repository.SignupRepository;
+
+@Repository
 public class SignupService {
     
+    @Autowired
+    SignupRepository repository;
+
+    // データベースの値を登録
+    public void insert(User user) {
+        repository.save(user);
+    }
 }
